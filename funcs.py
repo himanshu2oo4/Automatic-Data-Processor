9b223b32b3100cf512a4c1b3e9db4c53dd218cd9
himanshu2oo4 <himanshugg29@gmail.com>
2023-12-01 13:48:18 +0530

    Adding some new features

--- funcs.py
+++ funcs.py
@@ -1,6 +1,19 @@
 import pandas as pd 
 import time 
 import numpy as np 
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso 
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score , mean_squared_error
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
 # def nullRemoval(df) :
 #     print(f'null values in your data : \n{df.isna().sum()}')
 #     ans = input('Wanna remove all null value records ? Y/n :  ')
@@ -25,14 +38,62 @@ def nullRemoval(df):
     if ans in ('Y' , 'y'):
         df.dropna(inplace = True)
         print(f'Removing Them -----~~*')
-        time.sleep(3)
     else :
         print('Removing the rows and columns which are completely null ')
         df.dropna(how = 'all' , inplace = True , axis = 1)
         df.dropna(how = 'all' , inplace = True , axis = 0)
         print(f'Removing Them -----~~*')
-        time.sleep(3)
     
+def scaling(df):
+    ans = input('Do you wanna scale your dataset ? Y/n')
+    if ans in ('Y' , 'y'):
+        scaler = StandardScaler()
+        df = scaler.fit_transform(df)
+        return df 
+    else :
+        print('Okaay moving to next step --* ')
+    
+def dimensionalityReduction(df):
+    ans = input('do you wanna reduce the dimension of your data ? Y/n')
+    if ans in('Y', 'y'):
+        pca = PCA(n_components = 2)
+        df = pca.fit_transform(df)
+        df = pd.DataFrame(df)
+        print(f'your data looks like : \n{df.head()}')
+    else :
+        print('Okaay moving to next step !-----**')
 
+
+
+        
  
+
+def ModelSelection(df):
+    inpt = input('Tell me the type of your data means Regression or Classification')
+    if inpt =='Regression':
+        models = {
+            'Linear Regression' : LinearRegression(),
+            'Lasso Regression' : Lasso(),
+            'Ridge Regression' : Ridge(),
+            'RandomForestRegressor' : RandomForestRegressor(),
+            'DecisionTreeRegressor' : DecisionTreeRegressor(),
+            'SVR' : SVR()  
+        }
+        DependentVar = input('Enter your output feature name : ')
+        print('Dividing your dataset in train and test split --- * ')
+        x_train , x_test , y_train , y_test = train_test_split(df.drop(DependentVar, axis =1 ), df[DependentVar] , train_size=0.7 , random_state=42)
+        for model_name , model in models.items():
+            print(f'\nModel name : {model_name} --------------------** \n')
+            model.fit(x_train , y_train)
+            y_pred = model.predict(x_test)
+            r2Score = r2_score(y_test  , y_pred)
+            mse = mean_squared_error(y_test , y_pred)
+            print(f'\nPredicted values : {y_pred}')
+            print(f'Model evaluation')
+            print(f'R2 score : {r2Score} \nMean_squared_error : {mse}')
+
+
+       
+        
+
     
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd 
-from funcs import nullRemoval
+from funcs import nullRemoval, scaling , dimensionalityReduction , ModelSelection
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 import sys 
 arguments = sys.argv
 import matplotlib.pyplot as plt 
@@ -11,44 +13,56 @@ df = pd.read_csv(data, encoding= 'unicode_escape')
 
 
 
-# while True :
-#     print('\n\n')
-#     print('----------- Enter your choice : ----------')
-#     print('''
-#     1. Remove the null values 
-#     2. q for quit 
-#     ''')
-#     inp = input('Enter your choice : ')
-#     match inp:
-#         case '1':
-#             nullRemoval(df)
-#         case 'q' :
-#             break 
-
-ls = []
-for i, j in zip(df.isna().sum().keys() ,df.isna().sum().values ):
-    if j > 0 :
+inpt = input('What type of model you are trying to build ? Supervised/unsupervised ')
+if inpt == 'Supervised' :
+    ls = []
+    for i, j in zip(df.isna().sum().keys() ,df.isna().sum().values ):
+        if j > 0 :
         # print(f'you have some missing values in column : {i} ')
-        ls.append(i)
-print(f'our automatic cleaner detects some missing values in columns : {ls}')
-nullRemoval(df)
+            ls.append(i)
+    print(f'our automatic cleaner detects some missing values in columns : {ls}')
+    nullRemoval(df)
+    
+
+    # numericall check ---
+    print('for applying the ml model your data should be in a numerical format !! ')
+    print('We are getting the columns which have only numerical values in it ')
+    ls1 = []
+    for i , j in zip(df.dtypes.index ,df.dtypes.values):
+        if j != 'object':
+            ls1.append(i)
+    print(f'our automatic cleaner detects columns which have numerical vlaues are : {ls1}')
+    inp = input('Do you wanna remove columns other than this ? Y/n')
+    notls = [i for i in df.columns if i not in ls1]
+    if inp in('Y','y'):
+        df.drop(notls , axis = 1 , inplace = True)
+    else :
+        print('Okaay lets move on to next step !--- *')
+
+
+    print(f'Your final Dataframe : \n{df.head()}')
+    print(f'Dimension of your data : {df.shape}')
+
+    # Scaling the dataset
+    scaling(df)
+    ModelSelection(df)
+
+
+
+
     # df.dropna(inplace = True)
     # print('Null values get removed from your data !')
 
 # plt.bar(x= df['Gender'].value_counts().index , height =df['Gender'].value_counts().values  )
 
 
-print(sns.countplot(x = df['Gender']))
-plt.show()
 
 
-print(f'Your final Dataframe : \n{df.head()}')
 pcommand = input('You wanna save this into a CSV file ? Y/n :  ')
 if pcommand in ('Y' , 'y'):
     df.to_csv('Results.csv')
     print('Check the directory ! you get your file there !! ')
 else :
-    pass 
-
+    print('Okaay byee ')
 
 
