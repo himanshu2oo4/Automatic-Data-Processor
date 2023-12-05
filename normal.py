132dc86f62a3a81a30732a4f38df200d2f787de5
himanshu2oo4 <himanshugg29@gmail.com>
2023-12-05 16:36:54 +0530

    added requirements.txt file

--- normal.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-from sklearn.datasets import load_iris
-data = load_iris()
-df = pd.DataFrame(data.data , columns= data.feature_names)
-df['target'] = data.target
-df.to_csv('IrisData.csv')
-print('done')
